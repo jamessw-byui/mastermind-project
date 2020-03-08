a63eaa6c130c1d4ef0508712ab3f626a40a00a85
Jim White <jamessw@byui.edu>
2020-03-07 21:44:09 -0700

    prove09

--- index.js
+++ index.js
@@ -2,9 +2,45 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.get('/getRate', function (req, res) {
+	var weight = Math.ceil(req.query.weight);
+    var mailType = req.query.mailType;
+    var price = 0;
+    if(mailType === 'lettersStamped') {
+    	price = .40 + (weight * .15);
+    	price = price.toFixed(2);
+    } else if(mailType === 'lettersMetered') {
+    	price = .35 + (weight * .15);
+    	price = price.toFixed(2);
+    } else if(mailType === 'largeEnvelopesFlats') {
+    	price = .80 + (weight * .20);
+    	price = price.toFixed(2);
+    } else if(mailType === 'firstClassPackageServiceRetail') {
+    	if(weight < 5) {
+    		price = 3.80;
+    	} else if(weight < 9) {
+    		price = 4.60;
+    	} else if(weight < 13) {
+    		price = 5.30;
+    	} else if(weight >= 13) {
+    		price = 5.90;
+    	}
+    	price = price.toFixed(2);
+    }
+
+    res.render('pages/rates', {
+        weight: weight,
+        mailType: mailType,
+        price: price
+    });
+});
+
+
